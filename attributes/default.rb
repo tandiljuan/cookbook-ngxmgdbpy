d19e0a56b7c73e3ff8c453a3cd175001aeea14fe
Juan Manuel Lopez <tandiljuan@gmail.com>
2014-08-19 01:28:52 -0300

    Create `user` and `group` core attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,8 @@ default[:vagrant][:workspace_path] = '/opt/vboxsf/workspace'
 default[:vagrant][:workspace_nfs_path] = '/opt/nfs/workspace'
 
 # Core settings
+default[:core][:user]           = "vagrant"
+default[:core][:group]          = "vagrant"
 default[:core][:workspace_path] = '/opt/workspace'
 default[:core][:project_path]   = node[:core][:workspace_path]
 
